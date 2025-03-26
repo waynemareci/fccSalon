c89f765511bf5d61e1bad842e5085c9ad71fd8ce
waynemareci <wayne@mareci.com>
2025-03-26 13:02:01 +0000

    second break

--- salon.sh
+++ salon.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
 
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
 
-echo -e "Welcome to My Salon, how can I help you?" 
+echo -e "Welcome to My Salon, how can I help you?\n" 
 
 MAIN_MENU() {
   if [[ $1 ]]
@@ -11,15 +11,36 @@ MAIN_MENU() {
     echo -e "\n$1"
   fi
 
-  echo -e "\n1) cut\n2) color\n3) perm\n4) style\n5) trim"
+  SERVICE_MENU
+  
   read MAIN_MENU_SELECTION
 
   case $MAIN_MENU_SELECTION in
-    1) RENT_MENU ;;
-    2) RETURN_MENU ;;
-    3) EXIT ;;
+    1) MAKE_APPOINTMENT 1 ;;
+    2) MAKE_APPOINTMENT 2 ;;
+    3) MAKE_APPOINTMENT 3 ;;
+    4) MAKE_APPOINTMENT 4 ;;
+    5) MAKE_APPOINTMENT 5 ;;
     *) MAIN_MENU "I could not find that service. What would you like today?" ;;
   esac
+
+  exit
+}
+
+MAKE_APPOINTMENT() {
+  echo "inside make appointment; service_id is " $1
+
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
+}
+
+SERVICE_MENU(){
+  # get services
+  AVAILABLE_SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+  echo "$AVAILABLE_SERVICES" | while read SERVICE_ID BAR NAME
+  do
+    echo "$SERVICE_ID) $NAME"
+  done
 }
 
 MAIN_MENU
